232836404d7f5b4ac518429811ef3fa9cacc715b
ksugawara61 <katsuya@kokohadoko.net>
2018-12-30 23:49:42 +0900

    [add] 3.4.4 login form example

---
+++ 3/3.4.4/assets/js/app.js
@@ -0,0 +1,26 @@
+// コンポーネント定義
+Vue.component('user-login', {
+    template: '#login-template',
+    data: function () {
+        return {
+            userid: '',
+            password: ''
+        }
+    },
+    methods: {
+        login: function () {
+            auth.login(this.userid, this.password);
+        }
+    }
+})
+
+// ログイン周りのダミー
+var auth = {
+    login: function (id, pass) {
+        window.alert("userid:" + id + "\n" + "password:" + pass);
+    }
+}
+
+new Vue({
+    el: "#login-example"
+});
