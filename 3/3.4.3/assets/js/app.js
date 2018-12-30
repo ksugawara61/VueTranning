ccd8c3533a6a9ac29868ce1df20fa9e65796635c
ksugawara61 <katsuya@kokohadoko.net>
2018-12-30 23:37:00 +0900

    [add] 3.4.3 component slot

---
+++ 3/3.4.3/assets/js/app.js
@@ -0,0 +1,21 @@
+var headerTemplate = `
+    <div>
+        <slot name="header">No title</slot>
+    </div>
+`
+
+var contentTemplate = `
+    <div>
+        <slot name="content">No contents</slot>
+    </div>
+`
+Vue.component('page-header', {
+    template: headerTemplate
+})
+Vue.component('page-content', {
+    template: contentTemplate
+})
+
+new Vue({
+    el: '#fruits-list'
+})
