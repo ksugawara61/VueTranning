bbfd2a14cb887f626f0bbc50c059f6cdc4f30d6f
ksugawara61 <katsuya@kokohadoko.net>
2018-12-31 13:23:39 +0900

    [add] use VueRouter

---
+++ 4/4.2/app.js
@@ -0,0 +1,21 @@
+// ルートオプションを渡してルーターインスタンスを生成します
+var router = new VueRouter({
+    routes: [
+        {
+            path: '/top',
+            component: {
+                template: '<div>トップページです。</div>'
+            }
+        },
+        {
+            path: '/users',
+            component: {
+                template: '<div>ユーザー一覧ページです。</div>'
+            }
+        }
+    ]
+})
+
+var app = new Vue({
+    router: router
+}).$mount('#app')
