6f3be9d50436f739a2cac34bcb96ca5c86a84657
ksugawara61 <katsuya@kokohadoko.net>
2018-12-31 14:38:43 +0900

    [add] 4.4.3 詳細ページの実装

--- 4/4.2/app.js
+++ 4/4.2/app.js
@@ -50,6 +50,61 @@ var userList = {
     }
 }
 
+var userData = [
+    {
+        id: 1,
+        name: 'Takuya Tejima',
+        description: '東南アジアで働くエンジニアです。'
+    },
+    {
+        id: 2,
+        name: 'Yohei Noda',
+        description: 'アウトドア・フットサルが趣味のエンジニアです。'
+    }
+]
+
+var getUser = function (userId, callback) {
+    setTimeout(function () {
+        var filteredUsers = userData.filter(function (user) {
+            return user.id === parseInt(userId, 10)
+        })
+        callback(null, filteredUsers && filteredUsers[0])
+    }, 1000)
+}
+
+var userDetail = {
+    template: '#user-detail',
+    data: function () {
+        return {
+            loading: false,
+            user: null,
+            error: null
+        }
+    },
+
+    created: function () {
+        this.fetchData()
+    },
+
+    watch: {
+        '$route': 'fetchData'
+    },
+
+    methods: {
+        fetchData: function () {
+            this.loading = true
+            getUser (this.$route.params.userId, (function (err, user) {
+                this.loading = false
+                if (err) {
+                    this.error = err.toString()
+                } else {
+                    this.user = user
+                }
+            }).bind(this))
+        }
+    }
+}
+
 // ルートオプションを渡してルーターインスタンスを生成します
 var router = new VueRouter({
     routes: [
@@ -62,6 +117,10 @@ var router = new VueRouter({
         {
             path: '/users',
             component: userList
+        },
+        {
+            path: '/users/:userId',
+            component: userDetail
         }
     ]
 })
