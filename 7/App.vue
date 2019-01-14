51d27b973a5c42aae608900843010482cc408fd3
ksugawara61 <katsuya@kokohadoko.net>
2019-01-14 16:36:26 +0900

    [add] save and restore func

--- 7/App.vue
+++ 7/App.vue
@@ -39,6 +39,10 @@
                 フィルタしない
             </li>
         </ul>
+
+        <h2>保存と復元</h2>
+        <button type="button" v-on:click="save">保存</button>
+        <button type="button" v-on:click="restore">復元</button>
     </div>
 </template>
 
@@ -104,6 +108,16 @@ export default {
             this.$store.commit('changeFilter', {
                 filter: labelId
             });
+        },
+
+        // 現在の状態を保存
+        save() {
+            this.$store.dispatch('save');
+        },
+
+        // 保存されている状態を復元
+        restore() {
+            this.$store.dispatch('restore');
         }
     }
 }
